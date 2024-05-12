2ecc26a00d2b17f31f6829d08cffa0e3eda375a1
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-05-13 00:17:48 +1000

    Completed HeroBanner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Button, Stack, Typography } from "@mui/material";
+import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => {
   return (
@@ -14,15 +15,27 @@ const HeroBanner = () => {
       >
         FlexFit
       </Typography>
-      <Typography fontWeight={700} sx={{ fontSize: {lg: '44px', xs: '40px'}}}>
+      <Typography fontWeight={700} sx={{ fontSize: {lg: '44px', xs: '40px'}}} mb="23px" mt="30px">
         Lift, Sweat <br /> and Repeat
       </Typography>
       <Typography fontSize="22px" lineHeight="35px" mb={3}>
         Check out some of the most popular exercices.
       </Typography>
-      <Button variant="contained" color="error" href="#exercises">
+      <Button variant="contained" color="error" href="#exercises" sx={{backgroundColor: "#ff2625", padding: "10px"}}>
         Explore Exercises
       </Button>
+      <Typography
+        fontWeight={600}
+        color="#ff2625"
+        sx={{
+            opacity: 0.1,
+            display: { lg: 'block', xs: 'none'}
+        }}
+        fontSize="200px"
+      >
+        Train Hard!
+      </Typography>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
     </Box>
   );
 };
