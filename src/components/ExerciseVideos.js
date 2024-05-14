446794f10df37a2616d143a9308c304ab310e579
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-05-14 11:06:51 +1000

    Add Exercise Detail component

---
+++ src/components/ExerciseVideos.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Box, Typography, Stack } from "@mui/material";
+
+const ExerciseVideos = (props) => {
+  const { exerciseVideos, name } = props;
+  return (
+    <Box sx={{ marginTop: { lg: "200px", xs: "20px" } }} p="20px">
+      <Typography variant="h3" mb="33px">
+        Watch{" "}
+        <span style={{ color: "#ff2625", textTransform: "capitalize" }}>
+          {name}
+        </span>{" "}
+        videos on YouTube
+      </Typography>
+      <Stack
+        justifyContent="flex-start"
+        flexWrap="wrap"
+        alignItems="center"
+        sx={{ flexDirection: { lg: "row" }, gap: { lg: "110px", xs: "0" } }}
+      >
+        {exerciseVideos?.slice(0, 3).map((item, index) => (
+            <>
+            <iframe src={`https://www.youtube.com/embed/${item.video.videoId}`} width='375px' height='275px'/>
+            <Box>
+                <Typography variant='h5' color='#000'>
+                    {item.video.title}
+                </Typography>
+                <Typography variant='h6' color='#000'>
+                    {item.video.channelName}
+                </Typography>
+            </Box>
+            </>
+        ))}
+      </Stack>
+    </Box>
+  );
+};
+
+export default ExerciseVideos;
